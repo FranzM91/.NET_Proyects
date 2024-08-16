80dea16d96ec9efbda80925eac6ac2bb9e2bbfb3
Franz Mamani <franzmamanich1991@gmail.com>
2024-08-16 06:41:12 -0400

    updated proyect and added mappings, test proyect

---
+++ todoapp/uab.server/uab.server.test/TodoAppTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using uab.server.Business;
+using uab.server.Entities;
+
+namespace uab.server.test
+{
+    [TestClass]
+    public class TodoAppTest
+    {
+        private readonly TodoAppBusiness repositorio;
+        public TodoAppTest()
+        {
+            repositorio = new TodoAppBusiness();
+        }
+        [TestMethod]
+        public void Save()
+        {
+            var data = new TodoApp()
+            {
+                Descripcion = "test",
+                Estado = true,
+                Visible = true,
+                FechaCreacion = new DateTime(),
+                FechaActualizacion = new DateTime()
+            };
+
+            repositorio.SaveOrUpdate(data);
+
+            Assert.IsTrue(data.Id != 0);
+        }
+    }
+}
---
+++ todoapp/uab.server/uab.server/Business/TodoAppBusiness.cs
@@ -0,0 +1,50 @@
+﻿using uab.server.Data;
+using uab.server.Data.Contracts;
+using uab.server.Entities;
+
+namespace uab.server.Business
+{
+    public class TodoAppBusiness
+    {
+        private readonly ITodoAppDao todoAppDao;
+        public TodoAppBusiness()
+        {
+            todoAppDao = new TodoAppDao();
+        }
+
+        public TodoApp SaveOrUpdate(TodoApp data)
+        {
+            // opcion 1
+            var dato = new TodoApp();
+            if (data.Id == 0)
+            {
+                // nuevo dato
+                dato = todoAppDao.Save(data);
+            }
+            else
+            {
+                // actualizar dato
+                dato = todoAppDao.Update(data);
+            }
+            return dato;
+
+            // opcion 2
+            //return (data.Id == 0) 
+            //        ? todoAppDao.Save(data) 
+            //        : todoAppDao.Update(data);
+        }
+        public TodoApp GetById(int entityId)
+        {
+            return todoAppDao.GetById(entityId);
+        }
+        public void DeleteById(int entityId)
+        {
+            todoAppDao.DeleteById(entityId);
+        }
+
+        //public TodoApp ActualizarDato(TodoApp data)
+        //{
+        //    return todoAppDao.Update(data);
+        //}
+    }
+}
