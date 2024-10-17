df6563768162fac1b23dfc3d0923278d486621b9
Franz Mamani <franzmamanich1991@gmail.com>
2024-10-17 16:54:45 -0400

    update repository

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # .NET_Free
  All Basic Proyects
+## Error CORS
+# .NET Framework
+> Install-Package Microsoft.Owin.Cors
+# .NET Core
+> Install-Package Microsoft.AspNetCore.Cors
+
+## .NET CORE Agregar estas configuraciones en Program:
+ # OPTIONS
+ options.AddPolicy("AllowAnyOrigin", builder =>
+ {
+     builder.AllowAnyOrigin()
+         .AllowAnyMethod()
+         .AllowAnyHeader();
+ });
+
+# APP
+app.UseCors("AllowAnyOrigin");
+app.UseRouting();
--- todoapp_NetCore/uab.server.webapp/uab.server.webapp/Program.cs
+++ todoapp_NetCore/uab.server.webapp/uab.server.webapp/Program.cs
@@ -7,18 +7,29 @@ builder.Services.AddControllers();
 // enable allowAnyOrigin, allowAnyMethod, allowAnyHeader
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(builder =>
+    // TODO: FM implementar esta configuracion
+    options.AddPolicy("AllowAnyOrigin", builder =>
     {
         builder.AllowAnyOrigin()
             .AllowAnyMethod()
             .AllowAnyHeader();
     });
+    //options.AddDefaultPolicy(builder =>
+    //{
+    //    builder.AllowAnyOrigin()
+    //        .AllowAnyMethod()
+    //        .AllowAnyHeader();
+    //});
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// TODO: FM implementar esta configuracion
+app.UseCors("AllowAnyOrigin");
+app.UseRouting();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
